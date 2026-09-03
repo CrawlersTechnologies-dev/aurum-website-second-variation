50a3249e3a0d15ce6b2d3df8c380b4c92ab6e1f4
Priyanka-ch-dev <Priyankachannappahadapad@gmail.com>
2026-09-03 13:42:36 +0530

    updated tracking code

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import "./globals.css";
 import ClientLayout from "../components/ClientLayout";
+import Script from "next/script";
 
 export const metadata = {
   metadataBase: new URL("https://aurum-goldea.com"),
@@ -29,6 +30,11 @@ export const metadata = {
       "Live MyFXBook-verified AURUM Gold EA performance: +107.67% gain and $530k+ tracked profit.",
     images: ["/images/myfxbook-stats-profit-overview.png"],
   },
+  verification: {
+    other: {
+      "facebook-domain-verification": ["6jmss9h4bma99zovx6glkg1kgiq5q2"],
+    },
+  },
 };
 
 export const viewport = {
@@ -42,7 +48,32 @@ import { GoogleAnalytics } from '@next/third-parties/google';
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1225268763097069');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>
       <body suppressHydrationWarning>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1225268763097069&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ClientLayout>{children}</ClientLayout>
       </body>
       {process.env.NEXT_PUBLIC_GA_ID && <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_ID} />}
