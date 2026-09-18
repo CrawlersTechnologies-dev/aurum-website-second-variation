884265622830afe9bc00d6085ef75d890177d3d5
Priyanka-ch-dev <Priyankachannappahadapad@gmail.com>
2026-09-18 16:40:35 +0530

    added meta pixel data

--- src/app/layout.js
+++ src/app/layout.js
@@ -59,7 +59,7 @@ export default function RootLayout({ children }) {
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '1225268763097069');
+            fbq('init', '1122074216937828');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -70,7 +70,7 @@ export default function RootLayout({ children }) {
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=1225268763097069&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=1122074216937828&ev=PageView&noscript=1"
             alt=""
           />
         </noscript>
